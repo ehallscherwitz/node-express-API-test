7a3653b5a9f25b5d5115db8c2e8232d87aa65def
Ethan Scherwitz <ehallscherwitz@gmail.com>
2025-02-18 15:00:55 -0600

    changed local host to 0.0.0.0.

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const TEST = process.env.NODE_ENV === 'test';
 
-export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
+export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || '0.0.0.0'; // originally localhost
 export const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 12345;
 
 export const SERVER = {
